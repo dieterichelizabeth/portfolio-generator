bf3f862b99049c8ece6331243e5cbeae8e853539
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-15 22:21:34 -0600

    modularize the code

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 // statement to access fs module's functions
 const fs = require('fs');
 
+// assign object in module.export to generatePage
+const generatePage = require('./src/page-template.js');
+
 // Variable to hold user command-line arguments
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 // extract those arguments and store them into distinct variables
 const [name, github] = profileDataArgs;
-
-// (in functions w/ no paramaters, empty () are required)
-// arrow function that can receive input and display the data dynamically as a string
-// inserts them in a HTML template literal
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
 
 // create the HTML file from the string/template literal
 fs.writeFile('index.html', generatePage(name, github), err => {
---
+++ src/page-template.js
@@ -0,0 +1,25 @@
+// (in functions w/ no paramaters, empty () are required)
+// arrow function that can receive input and display the data dynamically as a string
+// inserts them in a HTML template literal
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+
+
+// related statements to use functions from one module inside another
+module.exports = generatePage;
