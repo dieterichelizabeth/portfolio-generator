c4f5f333ba54f82d2230a0dc6cc5c952fa3f00a7
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-02-16 15:33:06 -0600

    add .gitignore and inquirer

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
-// statement to access fs module's functions
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-// assign object in module.export to generatePage
-const generatePage = require('./src/page-template.js');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-// Variable to hold user command-line arguments
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // statement to access fs module's functions
+// const fs = require('fs');
 
-// extract those arguments and store them into distinct variables
-const [name, github] = profileDataArgs;
+// // assign object in module.export to generatePage
+// const generatePage = require('./src/page-template');
 
-// create the HTML file from the string/template literal
-fs.writeFile('index.html', generatePage(name, github), err => {
-  // if an error exists, an error message is displayed
-  if (err) throw err;
+// // extract those arguments and store them into distinct variables
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// // create the HTML file from the string/template literal
+// fs.writeFile('./index.html', pageHTML, err => {
+//    // if an error exists, an error message is displayed
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
